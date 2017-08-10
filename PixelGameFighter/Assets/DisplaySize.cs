7ce0d8b9535049238713fbaef4a52301dd082203
mobec <mobec622@outlook.jp>
2017-08-10 17:33:33 +0900

    Start
    FirstCommit

---
+++ PixelGameFighter/Assets/DisplaySize.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplaySize : MonoBehaviour {
+	int width, height;
+	// Use this for initialization
+	void Start () {
+		width = Screen.width;
+		height = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
