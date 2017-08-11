cdbce54a699dc90ab7db05d528f84fb9bf4fa6ea
mobec <mobec622@outlook.jp>
2017-08-11 18:33:14 +0900

    テキストのスーパークラス作成

---
+++ PixelGameFighter/Assets/SText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SText : MonoBehaviour {
+	int state_num;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		
+	}
+
+	public virtual void SetStateNum(int state_num_){
+		state_num = state_num_;
+	}
+}
