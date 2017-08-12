f74e10a28fd76ddd7cffe890ff59a75cb4457fb2
mobec <mobec622@outlook.jp>
2017-08-12 11:42:52 +0900

    難易度選択の文字色も変更

---
+++ PixelGameFighter/Assets/SStartSelectMenueText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SStartSelectMenueText : MonoBehaviour {
+	int state_num;
+	GameObject start_select_menue_system;
+	public StartSelectMenueSystem select_menue_system;
+
+	public Color select_color = new Color(0f / 255f, 0f / 255f, 0f / 255f);
+	public Color not_select_color = new Color(96f / 255f, 88f / 255f, 88f / 255f);
+	// Use this for initialization
+	public virtual void Start () {
+		start_select_menue_system = GameObject.Find("StartSelectMenueSystem");
+	}
+
+	// Update is called once per frame
+	public virtual void Update () {
+		select_menue_system  = start_select_menue_system.GetComponent<StartSelectMenueSystem>();
+	}
+
+	public void SetStateNum(int state_num_){
+		state_num = state_num_;
+	}
+
+	public int GetStateNum(){
+		return state_num;
+	}
+	
+}
---
+++ PixelGameFighter/Assets/StartSelectMenueSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSelectMenueSystem : MonoBehaviour {
+	int menue_state;
+	public enum DifficultyState{
+		Normal,
+		Hard,
+		Hell
+	};
+	// Use this for initialization
+	void Start () {
+		menue_state = (int)DifficultyState.Normal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ChangeMenueState();
+	}
+
+	public int GetMenueState(){
+		return menue_state;
+	}
+
+	void ChangeMenueState(){
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			menue_state++;
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			menue_state--;
+		}
+		if(menue_state > (int)DifficultyState.Hell){
+			menue_state = (int)DifficultyState.Normal;
+		}else if(menue_state < (int)DifficultyState.Normal){
+			menue_state = (int)DifficultyState.Hell;
+		}
+	}
+}
