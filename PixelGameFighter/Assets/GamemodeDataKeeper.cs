fc6e7c69f29edac58854e911cf873023b2d84f24
mobec <mobec622@outlook.jp>
2017-08-12 12:15:14 +0900

    シングルトンパターンの実装

---
+++ PixelGameFighter/Assets/GamemodeDataKeeper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamemodeDataKeeper {
+
+    private static GamemodeDataKeeper mInstance;
+
+	private GamemodeDataKeeper(){}
+
+    public static GamemodeDataKeeper Instance {
+
+        get {
+
+            if( mInstance == null ) mInstance = new GamemodeDataKeeper();
+
+            return mInstance;
+        }
+    }
+
+	int difficulty;
+    public int Difficulty{
+		set{difficulty = value;}
+		get{return difficulty;}
+	}
+}
---
+++ PixelGameFighter/Assets/TextDifficultyHard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDifficultyHard : SStartSelectMenueText {
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start();
+		base.SetStateNum((int)StartSelectMenueSystem.DifficultyState.Hard);
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update();
+		if(select_menue_system.GetMenueState() == base.GetStateNum()){
+			this.GetComponent<Text>().color = base.select_color;
+		}else{
+			this.GetComponent<Text>().color = base.not_select_color;
+		}
+	}
+}
