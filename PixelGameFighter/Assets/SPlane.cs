f0b042c11d2b0a6deaeadb93dce922e3f18f6037
mobec <mobec622@outlook.jp>
2017-08-14 17:01:09 +0900

    背景のスクロール

---
+++ PixelGameFighter/Assets/SPlane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPlane : MonoBehaviour {
+	Vector3 size;
+	const float move_speed = 5.0f;
+	// Use this for initialization
+	public virtual void Start () {
+		size = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		transform.position -= new Vector3(0.0f, move_speed, 0.0f);
+	}
+}
