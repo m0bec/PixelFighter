188cc607c7a3b8b600a4189df158289ac98a4293
mobec <mobec622@outlook.jp>
2017-08-17 23:05:47 +0900

    背景の出現位置の調整
    Fix 背景のつながりが不自然

--- PixelGameFighter/Assets/PlaneOne.cs
+++ PixelGameFighter/Assets/PlaneOne.cs
@@ -6,19 +6,26 @@ public class PlaneOne : SPlane {
 	GameFrame game_frame;
 	bool create_flag = false;
 	const float margin = 100.0f;
+	Vector3 this_plane_size;
+	public Vector3 ThisPlaneSize{
+		get{return this_plane_size;}
+	}
+	StageOneScrollSystem scroll_system;
 	// Use this for initialization
 	public override void Start () {
 		base.Start();
 		game_frame = GameObject.Find("GameFrame").GetComponent<GameFrame>();
+		scroll_system = GameObject.Find("StageOneScrollSystem").GetComponent<StageOneScrollSystem>();
+		this_plane_size = base.Size;
 	}
 	
 	// Update is called once per frame
 	public override void Update () {
 		base.Update();
-		if(this.transform.position.y < -base.Size.y && !create_flag){
+		if(this.transform.position.y < -scroll_system.PlanePosition.y && !create_flag){
 			Instantiate(
 				this,
-				new Vector3(base.Size.x, base.Size.y + game_frame.Height, base.Size.z),
+				new Vector3(scroll_system.PlanePosition.x, scroll_system.PlanePosition.y + game_frame.Height, scroll_system.PlanePosition.z),
 				base.Rotate);
 			create_flag = true;
 		}
--- PixelGameFighter/Assets/StageOneScrollSystem.cs
+++ PixelGameFighter/Assets/StageOneScrollSystem.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class StageOneScrollSystem : MonoBehaviour {
 	GameFrame game_frame;
 	float game_frame_width, game_frame_height;
+	Vector3 plane_position;
+	public Vector3 PlanePosition{
+		get{return plane_position;}
+	}
+	PlaneOne plane_one;
 	// Use this for initialization
 	void Start () {
 		game_frame = GameObject.Find("GameFrame").GetComponent<GameFrame>();
+		plane_one = GameObject.Find("PlaneOne").GetComponent<PlaneOne>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(plane_one != null){
+			plane_position = plane_one.ThisPlaneSize;
+		}
 	}
 }
