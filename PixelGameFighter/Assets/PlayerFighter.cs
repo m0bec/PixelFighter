7e4ed509e2a047a4babe299c6489c428e86dbef4
mobec <mobec622@outlook.jp>
2017-08-22 18:03:25 +0900

    ショットを打てるようになった

--- PixelGameFighter/Assets/PlayerFighter.cs
+++ PixelGameFighter/Assets/PlayerFighter.cs
@@ -19,6 +19,7 @@ public class PlayerFighter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Move();
+		Shot();
 	}
 
 	void GetObjecSize(){
@@ -53,5 +54,35 @@ public class PlayerFighter : MonoBehaviour {
 		}
 
 		this.transform.position = next_position;
+	}
+
+	public GameObject shot;
+	const float SHOT_RIGHT = 10.0f;
+	const float SHOT_LEFT = -10.0f;
+	bool shot_flag = false;
+	const float SHOT_COOL_TIME = 0.2f;
+	float shot_time = 0.0f;
+	void Shot(){
+			if(Input.GetKey(KeyCode.Space) && !shot_flag){
+				Instantiate(shot, new Vector3(
+					this.transform.position.x + SHOT_RIGHT,
+					this.transform.position.y,
+					this.transform.position.z + 1.0f),
+					Quaternion.identity);
+				Instantiate(shot, new Vector3(
+					this.transform.position.x + SHOT_LEFT,
+					this.transform.position.y,
+					this.transform.position.z + 1.0f),
+					Quaternion.identity);
+				shot_flag = true;
+			}
+
+			if(shot_flag){
+				shot_time += Time.deltaTime;
+				if(shot_time > SHOT_COOL_TIME){
+					shot_time = 0.0f;
+					shot_flag = false;
+				}
+			}
 	}
 }
