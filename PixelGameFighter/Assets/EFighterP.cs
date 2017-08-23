12bc5c6a5ccc767067893a402b19c11bab7d36ec
mobec <mobec622@outlook.jp>
2017-08-23 16:31:53 +0900

    レシプロエネミー追加

---
+++ PixelGameFighter/Assets/EFighterP.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EFighterP : MonoBehaviour {
+	const float ROLL_SPEED = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(new Vector3(0.0f, ROLL_SPEED, 0.0f));
+	}
+}
---
+++ PixelGameFighter/Assets/ETFighterG.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETFighterG : EnemyBase {
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start();
+		base.Score = 10;
+		base.Hp = 15.0f;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		
+	}
+}
