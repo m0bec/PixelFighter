0607d1904491499f55413f08490789398a8ff720
mobec <mobec622@outlook.jp>
2017-08-24 21:46:59 +0900

    Tankに当たり判定

---
+++ PixelGameFighter/Assets/TankUnder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankUnder : EnemyTankBase {
+	EnemyMove enemy_move;
+	// Use this for initialization
+	public override void Start () {
+		base.Start();
+		enemy_move = GetComponent<EnemyMove>();
+		enemy_move.Width = this.GetComponent<Renderer>().bounds.size.x;
+		enemy_move.Height = this.GetComponent<Renderer>().bounds.size.y;
+		base.Hp = 10.0f;
+		base.Score = 5;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.InField = enemy_move.InField;
+	}
+
+	public void SetState(int score_, float hp_){
+		base.Score = score_;
+		base.Hp = hp_;
+	}
+
+	public float GetWidth(){return enemy_move.Width;}
+	public float GetHeight(){return enemy_move.Height;}
+}
